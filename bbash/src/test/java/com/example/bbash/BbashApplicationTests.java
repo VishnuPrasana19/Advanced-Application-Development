05bb761548a23eab0f43ce1c623d1f38061522b8
VishnuPrasana19 <vishnusureshkumar10@gmail.com>
2024-03-26 21:54:07 +0530

    Day-5

---
+++ bbash/src/test/java/com/example/bbash/BbashApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.bbash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbashApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
